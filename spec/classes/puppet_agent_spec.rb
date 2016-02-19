0942eadf2a14623ae61523daaca0ee2ecbcd2830
Brandon High <brandon.high@puppetlabs.com>
2016-02-19 15:18:52 -0800

    (PE-11531) Add class relationships to specs

--- spec/classes/puppet_agent_spec.rb
+++ spec/classes/puppet_agent_spec.rb
@@ -41,11 +41,11 @@ describe 'puppet_agent' do
               it { is_expected.to contain_class('puppet_agent') }
               it { is_expected.to contain_class('puppet_agent::params') }
               it { is_expected.to contain_class('puppet_agent::prepare') }
-              it { is_expected.to contain_class('puppet_agent::install').that_comes_before('puppet_agent::service') }
+              it { is_expected.to contain_class('puppet_agent::install').that_requires('puppet_agent::prepare') }
               it { is_expected.to contain_package('puppet-agent').with_ensure('present') }
 
               if Puppet.version < "4.0.0"
-                it { is_expected.to contain_class('puppet_agent::service') }
+                it { is_expected.to contain_class('puppet_agent::service').that_requires('puppet_agent::install') }
                 if params[:service_names].nil?
                   it { is_expected.to contain_service('puppet') }
                   it { is_expected.to contain_service('mcollective') }
