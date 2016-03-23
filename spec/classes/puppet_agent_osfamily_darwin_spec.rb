6082ce1f6384c306cda6a1f9e418f918187dfc42
Brandon High <brandon.high@puppetlabs.com>
2016-03-23 10:56:40 -0700

    (PE-11531) Use iterator for OSX specs
    Prior to this commit we were repeating the same specs for
    each different supported OSX version.
    This commit uses an iterator based on the OS version tag to
    generate the tests for 10.9, 10.10 and 10.11.
    It also adds tests to ensure that old packages are forgotten.

--- spec/classes/puppet_agent_osfamily_darwin_spec.rb
+++ spec/classes/puppet_agent_osfamily_darwin_spec.rb
@@ -49,61 +49,53 @@ describe 'puppet_agent', :unless => Puppet.version < "3.8.0" do
   end
 
   describe 'supported environment' do
-    context "when OSX 10.9" do
-      let(:facts) do
-        facts.merge({
-          :is_pe                       => true,
-          :platform_tag                => "osx-10.9-x86_64",
-          :macosx_productversion_major => '10.9',
-        })
+    context "when running a supported OSX" do
+      ["osx-10.9-x86_64", "osx-10.10-x86_64", "osx-10.11-x86_64"].each do |tag|
+        let(:osmajor) { tag.split('-')[1] }
+
+        let(:facts) do
+          facts.merge({
+            :is_pe                       => true,
+            :platform_tag                => tag,
+            :macosx_productversion_major => osmajor,
+          })
+        end
+
+        it { should compile.with_all_deps }
+        it { is_expected.to contain_file('/opt/puppetlabs') }
+        it { is_expected.to contain_file('/opt/puppetlabs/packages') }
+        it { is_expected.to contain_file("/opt/puppetlabs/packages/puppet-agent-#{package_version}-1.osx#{osmajor}.dmg") }
+        it { is_expected.to contain_package('puppet-agent').with_ensure('present') }
+        it { is_expected.to contain_package('puppet-agent').with_source("/opt/puppetlabs/packages/puppet-agent-#{package_version}-1.osx#{osmajor}.dmg") }
+        it { is_expected.to contain_class('puppet_agent::install::remove_packages') }
+        it { is_expected.to contain_class('puppet_agent::install::remove_packages_osx') }
+        it { is_expected.to contain_class("puppet_agent::osfamily::darwin") }
+
+        if Puppet.version < "4.0.0"
+          [
+            'pe-augeas',
+            'pe-ruby-augeas',
+            'pe-openssl',
+            'pe-ruby',
+            'pe-cfpropertylist',
+            'pe-facter',
+            'pe-puppet',
+            'pe-mcollective',
+            'pe-hiera',
+            'pe-puppet-enterprise-release',
+            'pe-stomp',
+            'pe-libyaml',
+            'pe-ruby-rgen',
+            'pe-deep-merge',
+            'pe-ruby-shadow',
+          ].each do |package|
+            it { is_expected.to contain_exec("forget #{package}").with_command("/usr/sbin/pkgutil --forget com.puppetlabs.#{package}") }
+            it { is_expected.to contain_exec("forget #{package}").with_require('File[/opt/puppet]') }
+          end
+        else
+          it { is_expected.to contain_exec("forget #{package}").with_command("/usr/sbin/pkgutil --forget com.puppetlabs.#{package}") }
+        end
       end
-
-      it { should compile.with_all_deps }
-      it { is_expected.to contain_file('/opt/puppetlabs') }
-      it { is_expected.to contain_file('/opt/puppetlabs/packages') }
-      it { is_expected.to contain_file("/opt/puppetlabs/packages/puppet-agent-#{package_version}-1.osx10.9.dmg") }
-      it { is_expected.to contain_package('puppet-agent').with_ensure('present') }
-      it { is_expected.to contain_package('puppet-agent').with_source("/opt/puppetlabs/packages/puppet-agent-#{package_version}-1.osx10.9.dmg") }
-      it { is_expected.to contain_class('puppet_agent::install::remove_packages') }
-      it { is_expected.to contain_class("puppet_agent::osfamily::darwin") }
-    end
-
-    context "when OSX 10.10" do
-      let(:facts) do
-        facts.merge({
-          :is_pe                       => true,
-          :platform_tag                => "osx-10.10-x86_64",
-          :macosx_productversion_major => '10.10',
-        })
-      end
-
-      it { should compile.with_all_deps }
-      it { is_expected.to contain_file('/opt/puppetlabs') }
-      it { is_expected.to contain_file('/opt/puppetlabs/packages') }
-      it { is_expected.to contain_file("/opt/puppetlabs/packages/puppet-agent-#{package_version}-1.osx10.10.dmg") }
-      it { is_expected.to contain_package('puppet-agent').with_ensure('present') }
-      it { is_expected.to contain_package('puppet-agent').with_source("/opt/puppetlabs/packages/puppet-agent-#{package_version}-1.osx10.10.dmg") }
-      it { is_expected.to contain_class('puppet_agent::install::remove_packages') }
-      it { is_expected.to contain_class("puppet_agent::osfamily::darwin") }
-    end
-
-    context "when OSX 10.11" do
-      let(:facts) do
-        facts.merge({
-          :is_pe                       => true,
-          :platform_tag                => "osx-10.11-x86_64",
-          :macosx_productversion_major => '10.11',
-        })
-      end
-
-      it { should compile.with_all_deps }
-      it { is_expected.to contain_file('/opt/puppetlabs') }
-      it { is_expected.to contain_file('/opt/puppetlabs/packages') }
-      it { is_expected.to contain_file("/opt/puppetlabs/packages/puppet-agent-#{package_version}-1.osx10.11.dmg") }
-      it { is_expected.to contain_package('puppet-agent').with_ensure('present') }
-      it { is_expected.to contain_package('puppet-agent').with_source("/opt/puppetlabs/packages/puppet-agent-#{package_version}-1.osx10.11.dmg") }
-      it { is_expected.to contain_class('puppet_agent::install::remove_packages') }
-      it { is_expected.to contain_class("puppet_agent::osfamily::darwin") }
     end
   end
 end
